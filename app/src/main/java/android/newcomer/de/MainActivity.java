a195d3b6def6cb85ad07f1e4da50b37b9c9a565b
bruchsbergianer <60103404+bruchsbergianer@users.noreply.github.com>
2021-03-18 14:35:58 +0100

    tts eingebaut

--- app/src/main/java/android/newcomer/de/MainActivity.java
+++ app/src/main/java/android/newcomer/de/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
     private TextToSpeech tts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tts = new TextToSpeech(this, this);
+    }
+
+    @Override
+    protected void onInit(int status) {
+        tts.speak("Hallo!", TextToSpeech.QUEUE_FLUSH, null);
     }
 }
